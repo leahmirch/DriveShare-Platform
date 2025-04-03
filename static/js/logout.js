c87503bb18651e54dd26b30b9a45e0ab663128c4
zaynabmourtada <zaynabmo@umich.edu>
2025-04-03 03:13:03 -0400

    Deleted irrelevant files

--- static/js/logout.js
+++
@@ -1,6 +0,0 @@
-import mediator from './uiMediator';
-
-document.getElementById('logoutBtn').addEventListener('click', async () => {
-    await fetch('/logout');
-    mediator.notify(null, 'logout');
-});
